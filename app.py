4631d65f70326f16503579cfa6c94d32b5dd0411
Andrew Giannettino <andrewgiannettino@Andrews-MacBook-Air-2.local>
2024-05-09 16:39:46 -0500

    Started on Climate App

--- app.py
+++ app.py
@@ -1,30 +1,77 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Base.classes.keys()
+M = Base.classes.measurement
+S = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List All Avalible Routes"""
+    return(
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"api/v1.0/Tobs")
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    
+    prcp_results = session.query(M.date).filter(M.date > "2016-08-23").all()
+    session.close()
+    prcp = list(np.ravel(prcp_results))
+    return jsonify(prcp)
+
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    """List of Stations"""
+    results = session.query(S.station, S.name).all()
+    session.close()
+    all_stations = list(np.ravel(results))
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    tobs_results = session.query()    
+    
+if __name__ == "__main__":
+    app.run()
+
+    
